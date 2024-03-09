67980df40cc84922f31b605d68acaeb4252b8499
T00few2 <christian.kjar@gmail.com>
2024-03-09 09:31:29 +0100

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import HeroSection from "@/components/HeroSection";
-import Features from "@/components/Features";
 import Testimonials from "@/components/Footer";
 import { ColorModeScript } from '@chakra-ui/react'
 
