64f3857d73d94d27f8cf8649e4e3291d1290677e
T00few2 <christian.kjar@gmail.com>
2024-03-09 23:48:36 +0100

    some fixed

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,6 +3,8 @@
 import {
   Flex,
   Container,
+  chakra,
+  VisuallyHidden,
   Heading,
   Stack,
   Text,
@@ -10,6 +12,41 @@ import {
   Icon,
   IconProps,
 } from '@chakra-ui/react'
+
+import { FaFacebook, FaTwitter, FaYoutube, FaDiscord } from 'react-icons/fa'
+import { ReactNode } from 'react'
+
+const SocialButton = ({
+  children,
+  label,
+  href,
+}: {
+  children: ReactNode
+  label: string
+  href: string
+}) => {
+  return (
+    <chakra.button
+      bg={('#7289da')}
+      rounded={'full'}
+      w={10}
+      h={10}
+      cursor={'pointer'}
+      as={'a'}
+      href={href}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: ('white'),
+        color: ('#4267B2')
+      }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  )
+}
 
 export default function HeroSection() {
   return (
@@ -32,6 +69,15 @@ export default function HeroSection() {
         <Text color={'white'} maxW={'3xl'} as='cite' fontSize={{ base: 'l', sm: 'xl', md: '2xl' }}>
           the pain in the peleton
         </Text>
+
+        <Stack direction={'row'} spacing={6}>
+          <Button as ='a' colorScheme='facebook' href='https://www.facebook.com/groups/358114378652929' target="_blank" borderRadius="full">
+            <FaFacebook style={{ fontSize: '24px'}} />
+          </Button>
+          <Button as='a' bg='#7289da' href='https://discord.gg/WBtQ7J82JV' target ='_blank'>
+          <FaDiscord style={{ fontSize: '24px', color: 'white' }} />
+          </Button>
+        </Stack>
         
       </Stack>
     </Container>
