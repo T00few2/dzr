c9d11b722ae0064d7a50815404fcce4f9ac3a563
T00few2 <christian.kjar@gmail.com>
2024-03-14 09:48:23 +0100

    cards finallty fixed!

--- components/Features.tsx
+++ components/Features.tsx
@@ -21,56 +21,10 @@ import {
   SimpleGrid,
   SimpleGridProps,
 } from '@chakra-ui/react'
-import { ReactElement } from 'react'
-
-
 
 import { LiaMountainSolid } from "react-icons/lia";
 import { Im500Px } from "react-icons/im";
 
-/*
-interface CardProps {
-  heading: string
-  description: string
-  icon: ReactElement
-  href: string
-}
-
-const Card = ({ heading, description, icon, href }: CardProps) => {
-  return (
-    <Link href={href}>
-    <Box
-      maxW={{ base: 'full', md: '275px' }}
-      w={'full'}
-      borderWidth="5px"
-      borderRadius="lg"
-      overflow="hidden"
-      p={4}
-      flex='1'
-      >
-      <Stack align={'start'} spacing={5}>
-        <Flex
-          w={16}
-          h={16}
-          align={'center'}
-          justify={'center'}
-          rounded={'full'}
-          bg={('white')}>
-          {icon}
-        </Flex>
-        <Box mt={2}>
-          <Heading size="md" color={'white'}>{heading}</Heading>
-          <Text mt={1} fontSize={'sm'} color={'white'} whiteSpace="pre-line">
-            {description}
-          </Text>
-        </Box>
-
-      </Stack>
-    </Box>
-    </Link>
-  )
-}
-*/
 export default function Features() {
   return (
     <Box p={0}>
@@ -87,7 +41,7 @@ export default function Features() {
       <Stack  spacing={4} as={Container} maxW={'3xl'} mt={12} mb={20}>
       <SimpleGrid spacing={8}  minChildWidth='120px' alignContent={'center'}>
       <Link href={'dzr-after-party'} style={{ textDecoration: 'none' }}>
-          <Card bg={'black'} borderWidth="5px" borderRadius="lg" borderColor={'white'} _hover={{ transform: 'scale(1.05)', bg: "rgba(173, 26, 45, 0.95)"}}> 
+          <Card height='100%' bg={'black'} borderWidth="5px" borderRadius="lg" borderColor={'white'} _hover={{ transform: 'scale(1.05)', bg: "rgba(173, 26, 45, 0.95)"}}> 
             <CardHeader>
               <Flex justify="center" align="center" bg="white" borderRadius="full" p={2} width={16} height={16}>
                 <Icon as={LiaMountainSolid} w={10} h={10} color='black'/>
@@ -106,7 +60,7 @@ export default function Features() {
           </Card>
           </Link>
           <Link href={'the-zwifty-fifty'} style={{ textDecoration: 'none' }}>
-          <Card bg={'black'} borderWidth="5px" borderRadius="lg" borderColor={'white'} _hover={{ transform: 'scale(1.05)', bg: "rgba(173, 26, 45, 0.95)"}}> 
+          <Card height='100%' bg={'black'} borderWidth="5px" borderRadius="lg" borderColor={'white'} _hover={{ transform: 'scale(1.05)', bg: "rgba(173, 26, 45, 0.95)"}}> 
           <CardHeader>
             <Flex justify="center" align="center" bg="white" borderRadius="full" p={2} width={16} height={16}>
               <Icon as={Im500Px} w={10} h={10} color='black' />
