4fb444aca1d30a966434c37a54f3928092294f67
T00few2 <christian.kjar@gmail.com>
2024-03-17 23:04:11 +0100

    atom fix

--- app/api/feed/route.ts
+++ app/api/feed/route.ts
@@ -16,7 +16,7 @@ export async function GET(request: Request) {
     }));
   
     const feed = `<?xml version="1.0" encoding="UTF-8" ?>
-      <rss version="2.0">
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
           <channel>
               <title>DZR Racing Series</title>
               <link>https://dzrracingseries.com</link>
