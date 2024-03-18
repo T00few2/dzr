3d2b897d68c103742151df3e72a14a6c8ca62b9e
T00few2 <christian.kjar@gmail.com>
2024-03-18 20:54:37 +0100

    analytics again

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
         <Providers>
           <SidebarWithHeader />
           {children}
+          <Analytics />
         </Providers>
       </body>
     </html>
