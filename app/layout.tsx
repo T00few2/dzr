4cd0d31a1ddd5b48ede25fb5db1a5215c6b021e0
T00few2 <christian.kjar@gmail.com>
2024-03-18 20:51:47 +0100

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import SidebarWithHeader from "@/components/Sidebar";
+import { Analytics } from "@vercel/analytics/react"
 
 
 
