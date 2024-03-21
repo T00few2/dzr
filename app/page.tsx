7060c56bc0e8ec1176e37e65a7418ebdb387db90
T00few2 <christian.kjar@gmail.com>
2024-03-21 12:05:19 +0100

    MetaData fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,37 @@ import { Inter } from "next/font/google";
 import HeroSection from "@/components/HeroSection";
 import Features from "@/components/Features";
 import GoogleCalendarEmbed from "@/components/GoogleCalendar";
+import { Metadata } from "next";
 
 import { ColorModeScript } from '@chakra-ui/react'
 
-import Meta from '../components/Meta';
-
-
 const inter = Inter({ subsets: ['latin']})
+
+export const metadata: Metadata = {
+  title: 'DZR',
+  description: 'Danish Zwift Racers',
+
+  metadataBase: new URL('https://www.dzrracingseries.com/'),
+
+  openGraph: {
+    title: 'DZR',
+    description: 'Danish Zwift Racers',
+    url: 'https://www.dzrracingseries.com/',
+    siteName: 'DZR',
+    images: [
+      {
+        url: '/general/DZR_logo.svg'
+      }
+    ],
+    type: 'website',
+    locale: 'en_US',
+  }
+}
 
 export default function Home() {
   return (
     <>
-    <Meta
-    title="DZR"
-    description="Danish Zwift Racers"
-    frontPageImage="/DZR_logo.svg"
-    />
+
     <div style={{backgroundColor:'black'}}>
     <HeroSection />
     <Features />
