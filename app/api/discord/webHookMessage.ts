a52dbb901f5f348fd77dfe28efe27587a7e7ad4f
T00few2 <christian.kjar@gmail.com>
2024-03-21 23:04:56 +0100

    kjk

---
+++ app/api/discord/webHookMessage.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export async function GET(request: Request) {
+    const webhookUrl = 'https://discord.com/api/webhooks/1220454320777072721/WUKlYyFtiTRgJvOBM3e3l7uU3hX4XBNiylzNgKrSfu2SPOWWUwdPOebeLmoWGyC1hA78'
+    const message = 'Todays profile!'
+    const imageUrl ='https://dzrracingseries.com/dzr-after-party/libby-hill-after-party+/Libby-Hill-After-Party+-profile.png'
+    
+    try {
+        await axios.post(webhookUrl, {
+            content: message,
+            embeds: [
+                {
+                    image: {
+                        url: imageUrl
+                    }
+                }
+            ]
+        });
+        console.log('Message with image sent to Discord successfully!');
+    } catch (error) {
+        console.error('Error sending message to Discord:', error);
+    }
+    return new Response('Hello')
+}
