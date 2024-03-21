ffb629c47a453507219cf90da491c38dcd3d834e
T00few2 <christian.kjar@gmail.com>
2024-03-21 22:54:45 +0100

    wh

--- app/api/discord/webHookMessage.ts
+++ app/api/discord/webHookMessage.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-export async function sendDiscordMessage(webhookUrl: string, message: string, imageUrl: string): Promise<void> {
+export async function GET(request: Request)
+    {
+    const webhookUrl = 'https://discord.com/api/webhooks/1220454320777072721/WUKlYyFtiTRgJvOBM3e3l7uU3hX4XBNiylzNgKrSfu2SPOWWUwdPOebeLmoWGyC1hA78'
+    const message = 'Todays profile!'
+    const imageUrl ='https://dzrracingseries.com/dzr-after-party/libby-hill-after-party+/Libby-Hill-After-Party+-profile.png'
     try {
         await axios.post(webhookUrl, {
             content: message,
@@ -17,5 +21,3 @@ export async function sendDiscordMessage(webhookUrl: string, message: string, im
         console.error('Error sending message to Discord:', error);
     }
 }
-
-sendDiscordMessage('https://discord.com/api/webhooks/1220454320777072721/WUKlYyFtiTRgJvOBM3e3l7uU3hX4XBNiylzNgKrSfu2SPOWWUwdPOebeLmoWGyC1hA78','Todays profile!','https://dzrracingseries.com/dzr-after-party/libby-hill-after-party+/Libby-Hill-After-Party+-profile.png')
