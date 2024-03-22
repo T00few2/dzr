20c3f096c1dddf0b0faea304690f0a8583aa2787
T00few2 <christian.kjar@gmail.com>
2024-03-22 13:37:15 +0100

    fix

--- app/api/discord/route.ts
+++ app/api/discord/route.ts
@@ -1,12 +1,12 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+
 import axios from 'axios';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: Request, res: Response) {
   // Handle GET request
   return new Response('Hello')
 }
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: Request, res: Response) {
   const webhookUrl = 'https://discord.com/api/webhooks/1220454320777072721/WUKlYyFtiTRgJvOBM3e3l7uU3hX4XBNiylzNgKrSfu2SPOWWUwdPOebeLmoWGyC1hA78'
   const message = 'Todays profile!'
   const imageUrl ='https://dzrracingseries.com/dzr-after-party/libby-hill-after-party+/Libby-Hill-After-Party+-profile.png'
@@ -23,9 +23,9 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
           ]
       });
       console.log('Message with image sent to Discord successfully!');
-      res.status(200).json({ message: 'Message with image sent to Discord successfully!' });
+      
   } catch (error) {
       console.error('Error sending message to Discord:', error);
-      res.status(500).json({ message: 'Error sending message to Discord' });
+      
   }
 }
