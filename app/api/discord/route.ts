9d4d1d9e66ee7982197303374160e32c7d8a6717
T00few2 <christian.kjar@gmail.com>
2024-03-22 14:20:55 +0100

    remove GET

--- app/api/discord/route.ts
+++ app/api/discord/route.ts
@@ -1,13 +1,8 @@
 
 import axios from 'axios';
 
-export async function GET(req: Request, res: Response) {
-  // Handle GET request
-  return new Response('Hello')
-}
-
 export async function POST(req: Request, res: Response) {
-  const webhookUrl = process.env.DISCORD_WEB_HOOK_DZR_AFTER_PARTY//'https://discord.com/api/webhooks/1220454320777072721/WUKlYyFtiTRgJvOBM3e3l7uU3hX4XBNiylzNgKrSfu2SPOWWUwdPOebeLmoWGyC1hA78'
+  const webhookUrl = process.env.DISCORD_WEB_HOOK_DZR_AFTER_PARTY
   if (!webhookUrl) {
     throw new Error('WEBHOOK_URL is not defined');
   }
