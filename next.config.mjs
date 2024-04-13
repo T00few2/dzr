4914758b9c9986bcc98ea92da4eb9b25a101c44a
T00few2 <christian.kjar@gmail.com>
2024-04-14 00:32:02 +0200

    python backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
+  rewrites: async () => {
     return [
       {
-        source: '/api/python/:path*',
-        destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5328/api/:path*'
+            : '/api/',
       },
-    ];
-  }
-};
+    ]
+  },
+}
 
-export default nextConfig
+
