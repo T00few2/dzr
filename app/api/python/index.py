cde756e1ae30d55834bb5e7aa459da636da0cda9
T00few2 <christian.kjar@gmail.com>
2024-04-13 22:53:42 +0200

    python backend

--- app/api/python/index.py
+++ app/api/python/index.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+@app.route('/oh', methods=['GET'])
+def oh():
+    return "Oh!"
+
 if __name__ == '__main__':
     app.run(port=5328)
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/python/:path*',
+        destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
+      },
+    ];
+  }
+};
 
-export default nextConfig;
-
-
+export default nextConfig
