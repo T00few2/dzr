4ae4b1b8e959c12efcac9b51095cb7e4af32bc45
T00few2 <christian.kjar@gmail.com>
2024-04-14 00:12:14 +0200

    python backend

--- app/api/python/index.py
+++ app/api/python/index.py
@@ -1,10 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET'])
 def hello_world():
     return "Hello, World!"
 
-@app.route('/oh')
+@app.route('/oh', methods=['GET'])
 def oh():
     return "Oh!"
+
+if __name__ == '__main__':
+    app.run(port=5328)
