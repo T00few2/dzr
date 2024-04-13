7677ce4bf0137c69aca7e6b9327c1eec6c0d4a8e
T00few2 <christian.kjar@gmail.com>
2024-04-13 23:05:30 +0200

    python

--- app/api/python/index.py
+++ app/api/python/index.py
@@ -1,10 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET'])
+@app.route('/hello')
 def hello_world():
     return "Hello, World!"
 
-@app.route('/oh', methods=['GET'])
+@app.route('/oh')
 def oh():
     return "Oh!"
