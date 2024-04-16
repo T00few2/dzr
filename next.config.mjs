d41ff0400069611566e4d9824a5f610f53514f3d
T00few2 <christian.kjar@gmail.com>
2024-04-16 17:31:19 +0200

    python new try

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
+  rewrites: async () => {
     return [
       {
-        source: '/api/python/:path*',
-        destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
+      },
+      {
+        source: "/docs",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/docs"
+            : "/api/docs",
+      },
+      {
+        source: "/openapi.json",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/openapi.json"
+            : "/api/openapi.json",
       },
     ];
-  }
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
