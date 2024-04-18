7951fa25d382ee2b1d3cdf4b3224f3976cf55619
T00few2 <christian.kjar@gmail.com>
2024-04-18 08:51:21 +0200

    Revert "python new try"
    This reverts commit d41ff0400069611566e4d9824a5f610f53514f3d.

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
+  async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
-      },
-      {
-        source: "/docs",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/docs"
-            : "/api/docs",
-      },
-      {
-        source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
+        source: '/api/python/:path*',
+        destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
       },
     ];
-  },
+  }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
