a7915d309d469a5c876dca1b95055cd7fd20e8d6
T00few2 <christian.kjar@gmail.com>
2025-01-16 20:44:55 +0100

    bug fix

--- app/utils/firebaseAdminConfig.js
+++ app/utils/firebaseAdminConfig.js
@@ -5,7 +5,7 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY//?.replace(/\\n/g, '\n'), // Replace escaped newlines
+      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Replace escaped newlines
     }),
     databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
   });
