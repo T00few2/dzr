9ae14c1537c616317d9a84fc4a44fb96af185751
T00few2 <christian.kjar@gmail.com>
2025-11-07 07:19:52 +0100

    member zone page cleanup

--- app/members-zone/page.tsx
+++ app/members-zone/page.tsx
@@ -1,27 +1,25 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useSession, signOut } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import FeaturesMembers from "@/components/FeaturesMembers";
 import LoadingSpinnerMemb from '@/components/LoadingSpinnerMemb';
 import { useRouter } from 'next/navigation';
-import EditProfileModal from '@/components/auth/EditProfileModal'; 
+ 
 
 import {
   Container,
   Heading,
   Text,
   Stack,
-  Button,
   Box,
   Divider,
-  Center, // Import Button
+   
 } from '@chakra-ui/react';
 
 const MembersZone = () => {
     const { data: session, status } = useSession();
     const router = useRouter();
-    const [isEditProfileModalOpen, setEditProfileModalOpen] = useState(false);
     const [realName, setRealName] = useState<string | null>(null);
 
     useEffect(() => {
@@ -56,23 +54,9 @@ const MembersZone = () => {
                 <Stack spacing={4} as={Container} maxW={'3xl'} textAlign={'center'}>
                     <Heading color='white'>Members Zone</Heading>
                     <Text color={'white'}>Welcome, {greetingName}</Text>
-                    <Center>
-                        <Button background="rgba(173, 26, 45, 0.95)" color={'white'} onClick={() => signOut({ callbackUrl: '/login' })}  >
-                            Log Out
-                        </Button>
-                        <Button background="rgba(0, 122, 255, 0.95)" color={'white'} onClick={() => setEditProfileModalOpen(true)} ml={4}>
-                            Edit Profile
-                        </Button>
-                    </Center>
                     <Divider mb={4}/>
                 </Stack>
                 <FeaturesMembers/>
-
-                {/* Edit Profile Modal */}
-                <EditProfileModal 
-                isOpen={isEditProfileModalOpen} 
-                onClose={() => setEditProfileModalOpen(false)} 
-                />
             </>
         ) : (
             <Text align='center' color='white'>You need to login.</Text>
